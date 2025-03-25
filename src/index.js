d54108190ca050ea3a9c119836a1afcc06e9b51a
Manisha Agrawal <manisha.agrawal@impelsys.com>
2025-03-25 21:26:02 +0530

    index.js updated

--- src/index.js
+++ src/index.js
@@ -2,21 +2,17 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";  // ✅ Import Routes and Route
 import "./index.css";
 import App from "./App";
 import Product from "./components/Product";
 import Customer from "./components/Customer";
 import Home from "./components/Home";
-import StudentFarm from './components/StudentFarm'
+import StudentFarm from './components/StudentFarm';
 import reportWebVitals from "./reportWebVitals";
 import "@fortawesome/react-fontawesome";
 
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-
 
 root.render(
   <React.StrictMode>
@@ -25,15 +21,11 @@ root.render(
         <Route path="/" element={<App />} />
         <Route path="/product" element={<Product />} />
         <Route path="/customer" element={<Customer />} />
-        <Route path="/Home" element={<Home />} />
-        <Route path="/StudentFarm" element={<StudentFarm />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/studentfarm" element={<StudentFarm />} />
       </Routes>
     </Router>
   </React.StrictMode>
 );
- 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
